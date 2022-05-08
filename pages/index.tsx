3f0b62ad963fa757ad3e65d241fbba9bdb83cc59
cocoburger <73986381+cocoburger@users.noreply.github.com>
2022-05-08 21:28:42 +0900

    넷플릭스 로고 및 헤더 추가. 헤더 css 추가(global.css) icons 설치

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
             <Head>
-                <title>Create Next App</title>
+                <title>Home - Netflix</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            Lets builds Netflix
+            <Header/>
+            <main>
+            {/* 배너 */}
+                <section>
+                {/*    Row */}
+                {/*    Row */}
+                {/*    Row */}
+                {/*    Row */}
+                {/*    Row */}
+                {/*    Row */}
+                </section>
+            </main>
+        {/*    Modal   */}
         </div>
     )
 }
